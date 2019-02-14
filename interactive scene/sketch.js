6fd137a41e824b9c7801990f9d78c0741c6b1ca7
wilsonli6 <wwl10j@gmail.com>
2019-02-14 15:17:52 -0600

    p5js uploaded

--- interactive scene/sketch.js
+++ interactive scene/sketch.js
@@ -6,10 +6,38 @@
 // - describe what you did to take this project "above and beyond"
 
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+
+let backgroundImage
+let adidas;
+let nike;
+let puma;
+let scalar;
+
+function preload() {
+  adidas = loadImage("assets/adidas.png");
 }
+function setup() {
+  backgroundImage = loadImage("assets/sneaker.png");
+  createCanvas(650,650);
+  scalar = 0.1;
+}
+
 function draw() {
-  background(220);
-  ellipse(mouseX, mouseY, 50);
+  // background(backgroundImage);
+  imageMode(CORNER);
+  image(backgroundImage, 0, 0, width, height);
+  
+  imageMode(CENTER);
+  image(adidas, mouseX, mouseY, adidas.width*scalar, adidas.height*scalar);
+}
+function keyPressed(){
+	if (key === "a" || key === "A"){
+		image(adidas, mouseX, mouseY, adidas.width*scalar, adidas.height*scalar);
+  }
+  if (key === "n" || key === "N"){
+    image(nike, mouseX, mouseY, nike.width*scalar, nike.height*scalar);
+  }
+    if (key === "p" || key === "P"){
+    image(puma, mouseX, mouseY, puma.width*scalar, puma.height*scalar);
+  }
 }
