a4ad84d0f64997c29baec02d733fa2883e9b7252
wilsonli6 <wwl10j@gmail.com>
2019-02-14 15:05:29 -0600

    First Assignment Created

---
+++ interactive scene/sketch.js
@@ -0,0 +1,15 @@
+// Interactive Scene
+// Wilson Li
+// February 14, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+function draw() {
+  background(220);
+  ellipse(mouseX, mouseY, 50);
+}
