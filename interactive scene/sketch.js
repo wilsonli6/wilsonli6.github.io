dc93755bb73249e1a30e2c9d1ca8125feca3d020
wilsonli6 <wwl10j@hotmail.com>
2019-03-06 19:40:51 -0600

    Update sketch.js

--- interactive scene/sketch.js
+++ interactive scene/sketch.js
@@ -33,12 +33,12 @@ function setup() {
   createCanvas(650,650);
   backgroundImage = loadImage("assets/sneaker.png");
   scalar = 0.1;
+  imageScalar = 0.2;
   //choose the first image to appear
   whichImage = "adidas";
   whichShoe = ["adidasShoe", "pumaShoe", "nikeShoe"];
   newShoe = random(whichShoe);
   newShoe;
-  imageScalar = 0.2;
   shoeX = width/2;
   shoeY = height/2;
   py = random(2,10);
@@ -51,7 +51,7 @@ function draw() {
   image(backgroundImage, 0, 0, width, height);
   imageMode(CENTER);
   shoeSwitch;
-  //move the logo
+  //display the logo
   if (whichImage === "adidas") {
     image(adidas, x, y, adidas.width*scalar, adidas.height*scalar);
   }
@@ -61,6 +61,7 @@ function draw() {
   if (whichImage === "puma") {
     image(puma, x, y, puma.width*scalar, puma.height*scalar);
   }
+  //move the logo
   if (keyIsDown(LEFT_ARROW)) {
     x -= 5;
   }
@@ -76,16 +77,20 @@ function draw() {
   if (keyIsDown(DOWN_ARROW)) {
     y += 5;
   }
+  //display the shoe
   if (whichShoe === "pumaShoe") {
+    imageScalar = 0.2;
     image(pumaShoe, shoeX, shoeY, pumaShoe.width * imageScalar, pumaShoe.height * imageScalar);
   }
   if (whichShoe === "nikeShoe") {
+    imageScalar - 0.9;
     image(nikeShoe, shoeX, shoeY, nikeShoe.width * imageScalar, nikeShoe.height * imageScalar)
   }
   if (whichShoe === "adidasShoe") {
+    imageScalar = 0.9;
     image(adidasShoe, shoeX, shoeY, adidasShoe.width * imageScalar, adidasShoe.height * imageScalar)
   }
-  // move the shoe
+  //move the shoe
   shoeX += px;
   shoeY += py;
   
@@ -97,26 +102,25 @@ function draw() {
   if (shoeY >= height || shoeY <=0) {
     py = -1 * py;
   }
-  //display the shoe
 
   //switch the shoe depending on the contact
   function shoeSwitch(){
     if (whichShoe === "pumaShoe" && whichImage === "puma" &&
-    (shoeX <= x + 5 && shoeY <= y + 5)) {
+    (shoeX <= (x + 5) && shoeY <= (y + 5))) {
       points += 1;
       newShoe;
     }
     else if (whichShoe === "nikeShoe" && whichImage === "nike" &&
-    (shoeX <= x + 5 && shoeY <= y+ 5)) {
+    (shoeX <= (x + 5) && shoeY <= (y + 5))) {
       points += 1;
       newShoe;
     }
     else if (whichShoe === "adidasShoe" && whichImage === "adidas" &&
-    (shoeX <= x + 5 && shoeY <= y + 5)) {
+    (shoeX <= (x + 5) && shoeY <= (y + 5))) {
       points += 1;
       newShoe;
     }
-    else if (shoeX <= x + 5 && shoeY <= y + 5){
+    else if (shoeX <= (x + 5) && shoeY <= (y + 5)){
       points = 0;
       newShoe;
     }
@@ -128,16 +132,13 @@ function keyPressed(){
 	if (key === "a" || key === "A"){
     whichImage = "adidas";
     scalar = 0.1;
-		// image(adidas, mouseX, mouseY, adidas.width*scalar, adidas.height*scalar);
   }
   if (key === "n" || key === "N"){
     whichImage = "nike";
     scalar = 0.25;
-    // image(nike, mouseX, mouseY, nike.width*scalar, nike.height*scalar);
   }
   if (key === "p" || key === "P"){
     whichImage = "puma";
     scalar = 0.16;
-    // image(puma, mouseX, mouseY, puma.width*scalar, puma.height*scalar);
   }
 }
