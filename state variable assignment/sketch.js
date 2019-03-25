f13210aaf6c7b39e15c96dde1ae7ff61a5b0b129
wilsonli6 <wwl10j@hotmail.com>
2019-03-24 18:18:00 -0600

    Almost Done
    I just need to finalize the boundaries for the net and display 'goal' when the ball goes into the net

--- state variable assignment/sketch.js
+++ state variable assignment/sketch.js
@@ -1,7 +1,7 @@
 // States Variable Assignment
 // Wilson Li
-// March 8th, 2019
-//Soccer Ball assignment
+// March 25th, 2019
+//Soccer Ball Assignment
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 //nature of code dot com vectors
@@ -15,7 +15,7 @@ let soccerBallSpeedX, soccerBallSpeedY;
 let soccerNet, soccerNetX, soccerNetY, soccerNetWidth, soccerNetHeight, soccerNetScalar;
 let backgroundImage;
 let directionOfMovement;
-let gravity, acceleration, velocity, ground;
+let gravity, acceleration, xVelocity, yVelocity, ground;
 
 function preload() {
   //load images
@@ -54,7 +54,8 @@ function setup() {
   soccerBallSpeedY = 0;
   gravity = 0.3;
   acceleration = 0;
-  velocity = 0;
+  yVelocity = 0;
+  xVelocity = 0;
 
   //soccer net measurements
   soccerNetScalar = 10;
@@ -86,6 +87,8 @@ function draw() {
     movePlayer();
     animatePlayer();
     ballIsKicked();
+    ballGravity();
+    boundaries();
   }
 }
 
@@ -125,13 +128,13 @@ function displayPlayer() {
 
 function movePlayer() {
   if (keyIsDown(LEFT_ARROW)) {
-    playerX -= 5;
+    playerX -= 10;
     directionOfMovement = "left";
     playerImage = player.facingLeft;
   }
 
   if (keyIsDown(RIGHT_ARROW)) {
-    playerX += 5;
+    playerX += 10;
     directionOfMovement = "right";
     playerImage = player.facingRight;
   }
@@ -151,49 +154,79 @@ function animatePlayer() {
   if (keyIsDown(32) && directionOfMovement === "right") {
     playerImage = player.kickingRight;
     setTimeout(stopKicking, 100);
-    //image(player.kickingRight, playerX, playerY, playerWidth, playerHeight);
   }
   else if (keyIsDown(32) && directionOfMovement === "left") {
     playerImage = player.kickingLeft;
     setTimeout(stopKicking, 100);
-    // image(player.kickingLeft, playerX, playerY, playerWidth, playerHeight);
   }
 } 
 
 function ballIsKicked() {
-  if (playerImage === player.kickingRight && Math.abs(playerX - soccerBallX) <= 126 && playerX - soccerBallX <= 50){
-    ballGravity();
+  if (playerImage === player.kickingRight && Math.abs(playerX - soccerBallX) <= 126 && playerX - soccerBallX <= 50
+  && Math.abs(playerY - soccerBallY) <=90){
     soccerBallSpeedX = 20;
     soccerBallX += soccerBallSpeedX;
+    acceleration = -5;
+    if (soccerBallY < ground) {
+      xVelocity = 15;
+    }
   }
-}
-if (playerImage === player.kickingLeft && Math.abs(playerX - soccerBallX) <= 126 && playerX - soccerBallX >=-50) {
-  ballGravity();
-  soccerBallSpeedX = -20;
-  soccerBallX += soccerBallSpeedX;
+  if (playerImage === player.kickingLeft && Math.abs(playerX - soccerBallX) <= 126 && playerX - soccerBallX >=-50
+  && Math.abs(playerY - soccerBallY) <=90) {
+    soccerBallSpeedX = -20;
+    soccerBallX += soccerBallSpeedX;
+    acceleration = -2;
+    //the left kick is not as powerful
+    if (soccerBallY < ground) {
+      xVelocity = -3;
+    }
+  }
 }
 
 function ballGravity() {
-  acceleration = -5;
-  velocity += acceleration;
-  soccerBallY += velocity;
+  yVelocity += acceleration;
+  soccerBallY += yVelocity;
+  soccerBallX += xVelocity;
 
   // physics
   acceleration = 0;
-  velocity += gravity;
+  yVelocity += gravity;
   if (soccerBallY > ground) {
     soccerBallY = ground;
-    velocity = 0;
+    //this allows for the ball to bounce
+    yVelocity = yVelocity * -0.3;
   }
-  // soccerBallY += velocity;
-  // velocity += gravity;
-  // if (soccerBallY > height) {
-  //  Multiplying by -0.95 instead of -1 slows the square 
-  //  down each time it bounces (by decreasing speed).  
-  //  This is known as a "dampening" effect and is a more 
-  //  realistic simulation of the real world (without it, 
-  //  a ball would bounce forever).
-  //   velocity = velocity * -0.95;
-  //   soccerBallY = height;
-  // }
+  if (soccerBallY >= ground) {
+    xVelocity = 0;
+  }
+}
+
+function boundaries() {
+  //so the ball can't leave the screen
+  if (soccerBallX > width) {
+    soccerBallX = width - soccerBallWidth;
+    xVelocity = xVelocity * -1;
+  }
+  if (soccerBallX < 0) {
+    soccerBallX = 0 + soccerBallWidth;
+    xVelocity = xVelocity * -1;
+  }
+  if (soccerBallY < (0 + soccerBallHeight/2)) {
+    yVelocity = yVelocity * -1;
+  }
+  //so the player can't leave the screen
+  if (playerX > width) {
+    playerX = width;
+  }
+  if (playerX < 0) {
+    playerX = 0 - playerX/2;
+  }
+  //net boundaries
+  if (soccerBallX >= soccerNetX && soccerBallY > 20){
+    xVelocity = 0;
+  }
+  if (soccerBallX >= soccerNetX && soccerBallY < 20) {
+    yVelocity = yVelocity * -0.95;
+    xVelocity = -4;
+  }
 }
