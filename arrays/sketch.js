00f2cb83a6973743e2a171c2b502e69f5c28315a
wilsonli6 <wwl10j@gmail.com>
2019-04-08 15:15:41 -0600

    Failed Translation of Grid

--- arrays/sketch.js
+++ arrays/sketch.js
@@ -12,38 +12,28 @@ let cellSize;
 let autoPlay;
 
 function setup() {
+  // translate(width/2, height/2);
   createCanvas(windowHeight, windowWidth);
-  grid = create2DArray(gridSize, gridSize);
-  cellSize = width/gridSize;
+  cellSize = 150;
 }
 
 function draw() {
+  translate(500, 200);
   background(255);
   displayGrid();
+  ellipse(500, 50, 50, 50);
 }
 
 function displayGrid() {
   for (let y = 0; y < gridSize; y++) {
     for (let x = 0; x < gridSize; x++) {
-      if (grid[y][x] === 0) {
-        fill(255);
-      }
-      else {
-        fill(0);
-      }
       rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
   }
 }
 
 
-function create2DArray(cols, rows) {
-  let emptyArray = [];
-  for (let i = 0; i < rows; i++) {
-    emptyArray.push([]);
-    for (let j = 0; j < cols; j++) {
-      emptyArray[i].push(0);
-    }
-  }
-  return emptyArray;
-}
+//function mousePressed() {
+//   let xcoord = floor(mouseX/cellSize);
+//   let ycoord = floor(mouseY/cellSize);
+// }
