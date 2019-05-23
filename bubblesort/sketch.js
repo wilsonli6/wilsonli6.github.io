7788314b8dba41eb7c200cb158b7ff5e0518dcaa
wilsonli6 <wwl10j@gmail.com>
2019-05-23 15:04:56 -0600

    Finished Demo

--- bubblesort/sketch.js
+++ bubblesort/sketch.js
@@ -1,15 +1,47 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Bubble Sort
 
+let theNumbers = [5, 15, 3, 8, 9, 1, 20, 7];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  console.log(selectionSort(theNumbers));
 }
 
 function draw() {
   background(220);
+}
+
+function bubbleSort(someArray) {
+  let swap = true;
+  while(swap) {
+    swap = false;
+    for(let i = 0; i < someArray.length - 1; i++) {
+      if (someArray[i] > someArray[i+1]) {
+        //swap
+        let temp = someArray[i];
+        someArray[i] = someArray[i+1];
+        someArray[i+1] = temp;
+        swap = true;
+      }
+    }
+  }
+  return someArray;
+}
+
+function selectionSort(someArray) {
+  for (let swapLocation = 0; swapLocation < someArray.length; swapLocation++) {
+    let smallestLocation = swapLocation;
+
+    //one pass
+    for (let i = swapLocation; i < someArray.length; i++) {
+      if (someArray[i] < someArray[smallestLocation]) {
+        smallestLocation = i;
+      }
+    }
+    //swap
+    let temp = someArray[swapLocation];
+    someArray[swapLocation] = someArray[smallestLocation];
+    someArray[smallestLocation] = temp;
+  }
+  return someArray;
 }
